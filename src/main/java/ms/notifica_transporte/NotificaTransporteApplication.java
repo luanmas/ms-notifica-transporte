60344217cea9352153f4ceb967647cbc512cab94
luanmas <luanmas27@gmail.com>
2024-10-25 16:06:10 -0300

    first commit

---
+++ src/main/java/ms/notifica_transporte/NotificaTransporteApplication.java
@@ -0,0 +1,13 @@
+package ms.notifica_transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/notifica_transporte/NotificaTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.notifica_transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificaTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
